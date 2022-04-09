da6eada0832cee30049c82a343a2eebef67ec1b5
William Hagan <williamhagan1728@yahoo.com>
2022-04-08 19:36:19 -0700

    add base code into index.js for api

--- index.js
+++ index.js
@@ -1,2 +1,50 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const cors = require("cors")
+const repoContext = require("./repository/repository-wrapper");
+const songValidate = require("./middleware/song-validation");
+const songLogger = require("./middleware/songLogger");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+
+app.get("/api/songs", (req, res) => {
+    const songs = repoContext.songs.findAllSongs();
+    return res.send(songs);
+});
+
+
+app.get("/api/songs/:id", (req, res) => {
+    const id = req.params.id;
+    const song = repoContext.songs.findSongById(id);
+    return res.send(song);
+});
+
+app.post("/api/songs", [songInput, songValidate], (req, res) => {
+    const newSong = req.body;
+    const addedSong = repoContext.songs.createSong(newSong);
+    return res.status(201).send(addedSong);
+})
+
+app.put("/api/songs/:id", [songValidate], (req, res) => {
+    const id = parseInt(req.params.id);
+    const songPropertiesToModify = req.body;
+    const songToUpdate = repoContext.songs.updateSong(id,songPropertiesToModify);
+    return res.send(songToUpdate);
+})
+
+
+app.delete("/api/songs/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const deletedSong = repoContext.songs.deleteSong(id);
+    return res.send(deletedSong);
+})
+
+
+const PORT = process.env.PORT || 5005;
+
+app.listen(PORT, ()=> {
+    console.log(`Server running! On PORT: ${PORT}`)
+});
